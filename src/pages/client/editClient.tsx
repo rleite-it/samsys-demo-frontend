3c5b1de3c2f46567ca6f24c40e5c46ac0ea65d35
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-03-06 15:41:39 +0000

    Update editClient component

--- src/pages/client/editClient.tsx
+++ src/pages/client/editClient.tsx
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Col, Row } from "reactstrap";
+import {
+	Col,
+	Row,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+	Button,
+	Alert,
+} from "reactstrap";
 import { ClientDTO } from "../../models/client/clientDTO";
 import { ClientEditDTO } from "../../models/client/clientEditDTO";
 import { MessagingHelper } from "../../models/helper/messagingHelper";
@@ -11,17 +20,15 @@ export default function EditClient() {
 	const { id } = useParams<{ id: string }>();
 	const [clientToUpdate, setClientToUpdate] = useState<ClientEditDTO>();
 	const [isActive, setIsActive] = useState<boolean>(true);
-
 	const [errorMessage, setErrorMessage] = useState<string>();
 	const [successMessage, setSuccessMessage] = useState<string>();
-
 	const clientService = new ClientService();
 
 	const get = async () => {
 		const resultGetClient: MessagingHelper<ClientDTO | null> =
 			await clientService.Get(Number(id));
 
-		if (resultGetClient.success == false) {
+		if (!resultGetClient.success) {
 			setErrorMessage(resultGetClient.message);
 			setSuccessMessage("");
 			return;
@@ -42,7 +49,7 @@ export default function EditClient() {
 		const resultUpdate: MessagingHelper<ClientDTO | null> =
 			await clientService.Update(Number(id), clientToUpdate!);
 
-		if (resultUpdate.success == false) {
+		if (!resultUpdate.success) {
 			setErrorMessage(resultUpdate.message);
 			setSuccessMessage("");
 			return;
@@ -58,84 +65,75 @@ export default function EditClient() {
 	}, []);
 
 	return (
-		<>
-			<div style={{ width: "100%" }}>
-				<Row>
-					<Col xl={12}>
-						<h1>Editar Cliente</h1>
-					</Col>
-				</Row>
-			</div>
-
-			<div style={{ width: "20%", marginTop: "2em", display: "inline-block" }}>
-				<Row>
-					<Col xl={6} style={{ textAlign: "right" }}>
-						<label>Nome: </label>
-					</Col>
-					<Col xl={6}>
-						<input
+		<div
+			className="edit-client-container"
+			style={{ display: "flex", flexDirection: "column", placeItems: "center" }}
+		>
+			<h1 className="text-center">Editar Cliente</h1>
+			<div className="form-container">
+				<Form
+					style={{
+						maxWidth: "600px",
+						marginTop: "3rem",
+					}}
+				>
+					<FormGroup style={{ textAlign: "left" }}>
+						<Label for="name">Nome:</Label>
+						<Input
 							type="text"
+							id="name"
 							value={clientToUpdate?.name ?? ""}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+							onChange={(e) =>
 								setClientToUpdate({ ...clientToUpdate, name: e.target.value })
 							}
 						/>
-					</Col>
-				</Row>
-
-				<Row>
-					<Col xl={6} style={{ textAlign: "right" }}>
-						<label>Contacto: </label>
-					</Col>
-					<Col xl={6}>
-						<input
+					</FormGroup>
+					<FormGroup style={{ textAlign: "left" }}>
+						<Label for="phoneNumber">Contacto:</Label>
+						<Input
 							type="text"
+							id="phoneNumber"
 							value={clientToUpdate?.phoneNumber ?? ""}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+							onChange={(e) =>
 								setClientToUpdate({
 									...clientToUpdate,
 									phoneNumber: e.target.value,
 								})
 							}
 						/>
-					</Col>
-				</Row>
-
-				<Row>
-					<Col xl={12}>
-						<button className="btnUpdateClient" onClick={update}>
-							Atualizar
-						</button>
-					</Col>
-				</Row>
-
-				<Row>
-					<ClientStatusComponent
-						id={Number(id)}
-						isActive={isActive}
-						xl={12}
-						style={{ width: "100%", marginTop: "1em" }}
-						setErrorMessage={setErrorMessage}
-						setSuccessMessage={setSuccessMessage}
-					/>
-				</Row>
-
+					</FormGroup>
+					<Button color="primary" onClick={update}>
+						Atualizar
+					</Button>
+				</Form>
+			</div>
+			<div
+				style={{
+					marginTop: "3rem",
+					display: "flex",
+					flexDirection: "column",
+					gap: "1rem",
+				}}
+			>
+				<ClientStatusComponent
+					xl={12}
+					id={Number(id)}
+					isActive={isActive}
+					style={{ marginTop: "1em" }}
+					setErrorMessage={setErrorMessage}
+					setSuccessMessage={setSuccessMessage}
+				/>
 				{errorMessage && (
-					<Row>
-						<Col xl={12} className="error">
-							{errorMessage}
-						</Col>
-					</Row>
+					<Alert color="danger" className="alert">
+						{errorMessage}
+					</Alert>
 				)}
-
 				{successMessage && (
-					<Row>
-						<Col xl={12} className="success">
-							{successMessage}
-						</Col>
-					</Row>
+					<Alert color="success" className="alert">
+						{successMessage}
+					</Alert>
 				)}
 			</div>
-		</>
+		</div>
 	);
 }
